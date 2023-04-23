1a2055a43033d0453b2408c250edea960af257b2
Diego D B Thedoldi <diegodallabt01@gmail.com>
2023-04-23 07:27:01 -0300

    Melhorias na implementação do cálculo

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,20 @@
 function calculate(type, value) {
     if(type === 'action') {
-        if(value === 'clear') {
-            document.getElementById("output").value = '';
-        }
-        if(value === '/' || value === '*' || value === '-' || value === '+') {
-            document.getElementById("output").value += value;
-        }
-
-        if(value === '=') {
-            var result = eval(document.getElementById("output").value);
-            document.getElementById("output").value = result;
+        switch (value) {
+            case 'clear':
+                document.getElementById("output").value = '';
+                break;
+            case '/':
+            case '*':
+            case '-':
+            case '.':
+            case '+':
+                document.getElementById("output").value += value;
+                break;
+            case '=':
+                var result = eval(document.getElementById("output").value);
+                document.getElementById("output").value = result;
+                break;
         }
 
     } else if(type === 'value') {
